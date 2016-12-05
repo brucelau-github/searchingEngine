6bfcbbc2ea2de2a2fbea668bdc5f4945c0579bb0
brucelau_github <brucelau.email@gmail.com>
2016-12-04 19:06:06 -0500

    add backgroundimage and link query page and index page

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,55 +1,38 @@
-$( function() {
-  var availableTags = [
-    "ActionScript",
-    "AppleScript",
-    "Asp",
-    "BASIC",
-    "C",
-    "C++",
-    "Clojure",
-    "COBOL",
-    "ColdFusion",
-    "Erlang",
-    "Fortran",
-    "Groovy",
-    "Haskell",
-    "Java",
-    "JavaScript",
-    "Lisp",
-    "Perl",
-    "PHP",
-    "Python",
-    "Ruby",
-    "Scala",
-    "Scheme"
-  ];
-  // $( "#user_input" ).autocomplete({
-  //   // source: availableTags
-  //   source:"http://localhost:8080/querykeys"
-  // });
-  $("#user_input").autocomplete({
-				delay: 500,
-				minLength: 3,
-				source: function(request, response) {
-					$.getJSON("http://localhost:8080/querykeys", {
-						// do not copy the api key; get your own at developer.rottentomatoes.com
-						term: request.term,
-					}, function(data) {
-            console.log(data);
-						response(data);
-					});
-				},
-				focus: function(event, ui) {
-					// prevent autocomplete from updating the textbox
-					// event.preventDefault();
-          // $(this).val(ui.item.label);
-				},
-				select: function(event, ui) {
-					// prevent autocomplete from updating the textbox
-					event.preventDefault();
-          $(this).val(ui.item.label);
-					// navigate to the selected item's url
-					// window.open(ui.item.url);
-				}
+$(function() {
+	// $( "#user_input" ).autocomplete({
+	// // source: availableTags
+	// source:"http://localhost:8080/querykeys"
+	// });
+	$("#user_input").autocomplete({
+		delay : 500,
+		minLength : 3,
+		source : function(request, response) {
+			$.getJSON("http://localhost:8080/querykeys", {
+				// do not copy the api key; get your own at
+				// developer.rottentomatoes.com
+				term : request.term,
+			}, function(data) {
+				console.log(data);
+				response(data);
 			});
-} );
+		},
+		focus : function(event, ui) {
+			// prevent autocomplete from updating the textbox
+			// event.preventDefault();
+			// $(this).val(ui.item.label);
+		},
+		select : function(event, ui) {
+			// prevent autocomplete from updating the textbox
+			event.preventDefault();
+			$(this).val(ui.item.label);
+			// navigate to the selected item's url
+			// window.open(ui.item.url);
+		}
+	});
+	$('#user_input').keypress(function (e) {
+		  if (e.which == 13) {
+			  window.location = "/query?keyword=" + $('#user_input').val();
+			 e.preventDefault();
+		  }
+		});
+});
